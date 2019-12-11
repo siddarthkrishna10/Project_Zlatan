aa89dadd6b8e9bc880596be9ccabc704662b49ea
Siddarth Krishna S <36948033+siddarthkrishna10@users.noreply.github.com>
2019-12-11 20:19:22 +0100

    Update BarGraph.py

--- Code/BarGraph.py
+++ Code/BarGraph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Reading the dataset into an object
-a = pd.read_csv('https://github.com/siddarthkrishna10/Project_Zlatan/blob/master/Datasets/ZlatanStat_Sheet.csv')
+a = pd.read_csv('https://github.com/siddarthkrishna10/Project_Zlatan/blob/master/Code/ZlatanStat_Sheet.csv')
 
 #Plotting a bar graph for Market Value and Transfer Fee
 a.plot(x='Club', y=['Market_Value', 'Transfer_Fee'], kind='bar')
